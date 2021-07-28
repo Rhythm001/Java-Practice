0a972afe53655d64b2e3a2e8f1a50830cd799ff6
Rhythm <76173406+Rhythm001@users.noreply.github.com>
2021-07-28 17:14:36 +0530

    Update DayOfTheWeekChallenge.java

--- DayOfTheWeekChallenge.java
+++ DayOfTheWeekChallenge.java
@@ -12,35 +12,28 @@ public class DayOfTheWeekChallenge {
 
 
     public static void printDayOfTheWeek(int day) {
-        String weekDay;
+       
         switch(day){
             case 0:
-                weekDay = "Sunday";
-                System.out.println(weekDay);
+                System.out.println("Sunday);
                 break;
             case 1:
-                weekDay = "Monday";
-                System.out.println(weekDay);
+                System.out.println("Monday");
                 break;
             case 2:
-                weekDay = "Tuesday";
-                System.out.println(weekDay);
+                System.out.println("Tuesday");
                 break;
             case 3:
-                weekDay = "Wednesday";
-                System.out.println(weekDay);
+                System.out.println("Wednesday");
                 break;
             case 4:
-                weekDay = "Thursday";
-                System.out.println(weekDay);
+                System.out.println("Thursday");
                 break;
             case 5:
-                weekDay = "Friday";
-                System.out.println(weekDay);
+                System.out.println("Friday");
                 break;
             case 6:
-                weekDay = "Saturday";
-                System.out.println(weekDay);
+                System.out.println("Saturday");
                 break;
             default:
                 System.out.println("Invalid Day");
